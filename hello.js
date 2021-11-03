17b2e3ec5f81f4b4e8d2c1b833bc1425595c93aa
Robert Gladue <robgladue@hotmail.com>
2021-11-03 16:27:23 +0000

    added updates to hello and loopylighthouse docs

--- hello.js
+++ hello.js
@@ -8,3 +8,10 @@ sayHello();
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
